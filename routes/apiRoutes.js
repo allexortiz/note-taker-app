4af91420372186b96d14c0dc5a7a73bf1b8df1bc
DESKTOP-VF9OCE7\allex <allexortiz@outlook.com>
2023-11-21 18:06:50 -0600

    completed POST route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,18 +1,29 @@
 const router = require('express').Router();
 const fs = require('fs');
 const path = require('path');
-const fileName = './db/db.json'
+const { v4: uuidv4 } = require('uuid');
+const fileName = require('../db/db.json')
 
 // GET route for fetching notes
 router.get('/notes', (req, res) => {
-    const data = fs.readFileSync(fileName, 'utf8');
+    const data = fs.readFileSync('db/db.json', 'utf8');
     const parsedData = JSON.parse(data);
     res.json(parsedData);
 });
 
 // POST route for saving notes
 router.post('/notes', (req, res) => {
-    res.json('success');
+    // fileName.push(req.body);
+    const { title, text  } = req.body;
+    const payload = {
+        title,
+        text,
+        id: uuidv4()
+    };
+    console.log(payload);
+    const newData = [...fileName, payload];
+    const data = fs.writeFileSync('db/db.json', JSON.stringify(newData), (err) => console.log(err));
+    res.json(data);
 });
 
 module.exports = router;
