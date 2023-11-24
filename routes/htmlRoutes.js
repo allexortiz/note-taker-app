11e9a46b9ebb01455ecc9e11e7961f9ca549efd2
DESKTOP-VF9OCE7\allex <allexortiz@outlook.com>
2023-11-23 20:00:45 -0600

    everything

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const path = require('path');
 
 // GET route for the homepage
-router.get('*', (req, res) =>
+router.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '../public/index.html'))
 );
 
